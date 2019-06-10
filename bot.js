bc853f96fe26ee08c3efaec065e4ed2773847b5c
DraColaTGS <50123084+DraColaTGS@users.noreply.github.com>
2019-06-10 10:35:19 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,12 +1,8 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
- 
- 
-
-  
-
-const client     = new Discord.Client();
 const prefix   = "^";
+
+
 
 client.on("message", message => {
   if (message.content === `${prefix}help`) {
