28f55fc99bee304bd9d6a2a3a44864130e66ddc0
DraColaTGS <50123084+DraColaTGS@users.noreply.github.com>
2019-06-10 10:07:32 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,131 +2,162 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
  
  
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-   client.user.setActivity("^help",{type: 'WATCHING'})
-  console.log('')
-  console.log('')
-  console.log('?[?????????????????????????????????????????????????????????????????]?')
-  console.log(`[Start] ${new Date()}`);
-  console.log('?[?????????????????????????????????????????????????????????????????]?')
-  console.log('')
-  console.log('?[????????????????????????????????????]?');
-  console.log(`Logged in as * [ " ${client.user.username} " ]`);
-  console.log('')
-  console.log('Informations :')
-  console.log('')
-  console.log(`servers! [ " ${client.guilds.size} " ]`);
-  console.log(`Users! [ " ${client.users.size} " ]`);
-  console.log(`channels! [ " ${client.channels.size} " ]`);
-  console.log('?[????????????????????????????????????]?')
-  console.log('')
-  console.log('?[????????????]?')
-  console.log(' Bot Is Online')
-  console.log('?[????????????]?')
-  console.log('')
-  console.log('')
-});
 
   
-client.on('message', message => {
+const Discord  = require('discord.js');
+const client     = new Discord.Client();
+const prefix   = "-";
 
-const prefix = '^'
-
-if (message.content.startsWith(prefix + 'help')) { //DiamondCodes - [ X_KillerYT ]
-    let pages = [`
-***__وصف عن البوت__***
+client.on("message", message => {
+  if (message.content === `${prefix}help`) {
+  const embed = new Discord.RichEmbed()
+      .setColor("#000000")
+      .setDescription(`
+   ** :bookmark:  Help bc :bookmark: **
+** ${prefix}bc / لــ ارسال برود كاسيت للكل
+   ${prefix}obc / لــ ارسال برود كاسيت لللاون لاين فقط 
+   ${prefix}rolebc / لــ ارسال برود كاست لرتبه معينه
+   ${prefix}sand / لـ ارسال برود كاسيت لشخص معين
+   ${prefix}bot / لمعرفه معلومات البوت
 **
-
-『^bc / لي ارسال برود كاسيت للكل  』
-
-『^obc/لي ارسال برود كاسيت للاونلاين 』
-  `
-,`
+==============================================
+** لتغير حلات البوت **
+** 
+   ${prefix}sp / لــ تغير حاله البوت الي بلنيق
+   ${prefix}sw / لـ تغير حاله البوت الي واتشنق
+   ${prefix}sl / لـ تغير حاله البوت الي لستينق
+   ${prefix}ss / لتغير حاله البوت الي استريمنق
+**
+`)
+   message.channel.sendEmbed(embed)
    
-   
-`]
-    let page = 1;
- 
-    let embed = new Discord.RichEmbed()
-    .setColor('RANDOM')
-    .setFooter(`Page ${page} of ${pages.length}`)
-    .setDescription(pages[page-1])
- 
-    message.author.sendEmbed(embed).then(msg => {
- 
-        msg.react('◀').then( r => {
-            msg.react('▶')
- 
- 
-        const backwardsFilter = (reaction, user) => reaction.emoji.name === '◀' && user.id === message.author.id;
-        const forwardsFilter = (reaction, user) => reaction.emoji.name === '▶' && user.id === message.author.id;
- 
- 
-        const backwards = msg.createReactionCollector(backwardsFilter, { time: 2000000});
-        const forwards = msg.createReactionCollector(forwardsFilter, { time: 2000000});
- 
- 
- 
-        backwards.on('collect', r => {
-            if (page === 1) return;
-            page--;
-            embed.setDescription(pages[page-1]);
-            embed.setFooter(`Page ${page} of ${pages.length}`);
-            msg.edit(embed)
-        })
-        forwards.on('collect', r => {
-            if (page === pages.length) return;
-     
-      page++;
-            embed.setDescription(pages[page-1]);
-            embed.setFooter(`Page ${page} of ${pages.length}`);
-            msg.edit(embed)
-        })
-        })
-    })
-    }
+   }
+   });
+
+const developers = ["439020996171923466"]
+const adminprefix = "-";
+client.on('message', message => {
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!developers.includes(message.author.id)) return;
+
+  if (message.content.startsWith(adminprefix + 'sp')) {//بلينق
+    client.user.setGame(argresult);
+      message.channel.send(`**Status You   ${argresult}:white_check_mark: **`)
+  } else
+  if (message.content.startsWith(adminprefix + 'sw')) {//واتشنق
+  client.user.setActivity(argresult, {type:'WATCHING'});
+      message.channel.send(`**Status You   ${argresult}:white_check_mark: **`)
+  } else
+  if (message.content.startsWith(adminprefix + 'sl')) {//لستينق
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.send(`**Status You  ${argresult} :white_check_mark: **`)
+  } else
+  if (message.content.startsWith(adminprefix + 'ss')) {//استريمنق
+    client.user.setGame(argresult, "https://www.twitch.tv/M3roof");
+      message.channel.send(`**Status You ${argresult}:white_check_mark:  **`)
+}
 });
-
 
 client.on('message', message => {
-    if(message.content === '^help') {
-        message.reply('تم ارساله بالخاص :white_check_mark: ');
+    if(message.content.startsWith(prefix + 'bot')) {
+    message.channel.send({
+        embed: new Discord.RichEmbed()
+            .setAuthor(client.user.username,client.user.avatarURL)
+            .setThumbnail(client.user.avatarURL)
+            .setColor('RANDOM')
+            .setTitle('``bot info`` ')
+            .addField('``My Ping``' , [`${Date.now() - message.createdTimestamp}` + 'MS'], true)
+            .addField('``RAM Usage``', `[${(process.memoryUsage().rss / 1048576).toFixed()}MB]`, true)
+            .addField('``servers``', [client.guilds.size], true)
+            .addField('``channels``' , `[ ${client.channels.size} ]` , true)
+            .addField('``Users``' ,`[ ${client.users.size} ]` , true)
+            .addField('``My Name``' , `[ ${client.user.tag} ]` , true)
+            .addField('``My ID``' , `[ ${client.user.id} ]` , true)
+                  .addField('``My Prefix``' , `[ - ]` , true)
+                  .addField('``My Language``' , `[ Java Script ]` , true)
+                  .setFooter('By |!LG ÐrĀčuĿĀ#6764')
+    })
+}
+});
+
+client.on("message", message => {  
+              if (message.content.startsWith(prefix + "obc")) {    
+                           if (!message.member.hasPermission("ADMINISTRATOR"))  return;   
+    let args = message.content.split(" ").slice(1);  
+    var argresult = args.join(' ');
+    message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {  
+   m.send(`${argresult}\n ${m}`);   
+  })
+   message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'offline').size}\` :mailbox:  عدد المستلمين `);
+   message.delete();
+  };    
+  });
+
+client.on('message', message => { 
+    if (message.author.id === client.user.id) return;
+    if (message.guild) {
+   let embed = new Discord.RichEmbed()
+    let args = message.content.split(' ').slice(1).join(' ');
+if(message.content.split(' ')[0] == prefix + 'bc') {
+    if (!args[1]) {
+message.channel.send("**^bc <message>**");
+return;
+}
+        message.guild.members.forEach(m => {
+   if(!message.member.hasPermission('ADMINISTRATOR')) return;
+            var bc = new Discord.RichEmbed()
+            .addField('» السيرفر :', `${message.guild.name}`)
+            .addField('» المرسل : ', `${message.author.username}#${message.author.discriminator}`)
+            .addField(' » الرسالة : ', args)
+            .setColor('#ff0000')
+            // m.send(`[${m}]`);
+            m.send(`${m}`,{embed: bc});
+        });
+    }
+    } else {
+        return;
     }
 });
- 
+
+
+				client.on('message' , message => {
+      if(message.author.bot) return;
+     
+      if(message.content.startsWith(prefix + "rolebc")) {
+        if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+        let args = message.content.split(" ").slice(2);
+     var codes = args.join(' ')
+       
+        if(!codes) {
+          message.channel.send("قم بكتابة الرسالة | !rolebc @everyone message")
+            return;
+        }
+     
+     
+              var role = message.mentions.roles.first();
+                if(!role) {
+                  message.reply("لا توجد رتبة بهذا الاسم")
+                    return;
+                }
+            message.guild.members.filter(m => m.roles.get(role.id)).forEach(n => {
+              n.send(
+              "**" + 
+             "\n" +
+              `${codes}` + "**"
+              )
+            })
+            message.channel.send(`**لقد تم ارسال هذه الرسالة الى ${message.guild.members.filter(m => m.roles.get(role.id)).size} عضو**`)
+        }
+    }); 
 
 client.on("message", message => {
+  if (message.content.startsWith(prefix + 'send')) {
+    if(!message.author.id === "569502505289908245") return;
+    var user = message.mentions.members.first();
+    var args = message.content.split(" ").slice(1).join(" ");
+user.send(args);
+  }});
 
-const prefix = '^'
- 
-            if (message.content.startsWith(prefix + "bc")) {
-                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
-  let args = message.content.split(" ").slice(1);
-  var argresult = args.join(' ');
-  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {//حقوق دايموند كودز
- m.send(`${argresult}\n ${m}`);
-})
- message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : عدد الاعضاء المستلمين`);
- message.delete();
-};    
-});
- 
-
-client.on("message", message => {
- 
- const prefix = '^'
-
-            if (message.content.startsWith(prefix + "obc")) {
-                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
-  let args = message.content.split(" ").slice(1);
-  var argresult = args.join(' ');
-  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {//حقوق دايموند كودز
- m.send(`${argresult}\n ${m}`);
-})
- message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : عدد الاعضاء المستلمين`);
- message.delete();
-};    
-});
+client.login(process.env.BOT_TOKEN);
  
 client.login(process.env.BOT_TOKEN);
